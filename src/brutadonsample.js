fded4b74ec1d027dcb1795912fce064329bc5547
Rosstin Murphy <asterazul@gmail.com>
2017-01-21 13:32:09 -0800

    delete brutadon sample

--- src/brutadonsample.js
+++
@@ -1,5 +0,0 @@
-var brutadonSample = [
-{'prompt' : "prompt blah blah one", 'wreck': "wreck one"},
-{'prompt' : "prompt blah blah two", 'wreck': "wreck two"},
-{'prompt' : "prompt blah three", 'wreck': "rekt three"}
-];
