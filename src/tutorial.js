a14d712c684467d601d79ca7c325aa75fab05153
wincelet <brookelfnichols@gmail.com>
2017-01-21 23:41:33 -0800

    typo fix

--- src/tutorial.js
+++ src/tutorial.js
@@ -1,6 +1,5 @@
 var tutorial = [
   {
-    "21": "",
     "prompt": "Welcome to, You Got This Brutadon, a game where you must use your voice in the name of true friendship. Make sure Alexa can hear you clearly, and make sure no one will be disturbed by your cacophonous shouting. If this is your first time playing, You Got This Brutadon, shout, PUMP IT UP BRUTADON, to begin the tutorial. If you would like to skip the tutorial, shout WRECK um BRUTADON.",
     "wreck": 2,
     "pump": 1,
@@ -9,6 +8,7 @@ var tutorial = [
     "none": 3,
     "huh": 3,
     "t1": "GOTO TUTORIAL",
+    "s1": "",
     "t2": "SKIP TUTORIAL AND GO STRAIGHT TO CORE GAME LOOP",
     "s2": "",
     "t3": "If this is your first time playing, You Got This Brutadon, shout, PUMP IT UP BRUTADON, to begin the tutorial. If you would like to skip the tutorial, shout WRECK um BRUTADON.",
@@ -21,7 +21,6 @@ var tutorial = [
     "s6": ""
   },
   {
-    "21": 1,
     "prompt": "We begin long ago, in middle school, when you and Brutadon first became fast friends. Brutadon was the only kaijew in your sixth grade class, and he towered seven stories above all the other students. When they made fun of him, you used to shout, wreckum, Brutadon!",
     "wreck": 1,
     "pump": 2,
@@ -30,6 +29,7 @@ var tutorial = [
     "none": 2,
     "huh": 2,
     "t1": "After Brutadon had killed twenty-five of your classmates, no one ever made fun of him again. He learned that you were the only one who truly had his back.",
+    "s1": 1,
     "t2": "When your classmates made fun of Brutadon, you used to shout, wreckum, Brutadon!",
     "s2": "",
     "t3": "",
@@ -42,7 +42,6 @@ var tutorial = [
     "s6": ""
   },
   {
-    "21": 1,
     "prompt": "Your friendship continued into adulthood. You became a personal trainer, and Brutadon came to rely on your coaching and support. Whenever he was training for the big kaijew fight that he knew was his destiny, you would shout, pump it up Brutadon!",
     "wreck": 2,
     "pump": 1,
@@ -51,6 +50,7 @@ var tutorial = [
     "none": 2,
     "huh": 2,
     "t1": "Brutadon pumped it up indeed. Soon, his seven biceps were each bigger than cement trucks, and his glutes thundered across the plains.",
+    "s1": 1,
     "t2": "Whenever Brutadon was training for the big kaijew fight that he knew was his destiny, you would shout, pump it up Brutadon!",
     "s2": "",
     "t3": "",
@@ -63,7 +63,6 @@ var tutorial = [
     "s6": ""
   },
   {
-    "21": 1,
     "prompt": "But his training didn't always go perfectly. Sometimes, Brutadon worried about whether he was really strong enough to meet his fate. Whenever he was feeling down, you'd tell him, you got this, Brutadon!",
     "wreck": 2,
     "pump": 2,
@@ -72,6 +71,7 @@ var tutorial = [
     "none": 2,
     "huh": 2,
     "t1": "With your emotional support, Brutadon could achieve anything. Before long, the whole world knew of Brutadon's strength, and he was very popular on social media.",
+    "s1": 1,
     "t2": "Whenever Brutadon was feeling down, you would tell him, you got this, Brutadon!",
     "s2": "",
     "t3": "",
@@ -84,7 +84,6 @@ var tutorial = [
     "s6": ""
   },
   {
-    "21": 1,
     "prompt": "Journalists and youtubers often made the trek to the airplane hanger in the middle of the desert where you and Brutadon trained for the big fight. Brutadon hated the press. When he got antsy about all those visitors, you'd urge him not to stomp on anybody. You said, hold back, Brutadon!",
     "wreck": 2,
     "pump": 2,
@@ -93,6 +92,7 @@ var tutorial = [
     "none": 2,
     "huh": 2,
     "t1": "You and Brutadon were a good team. Brutadon didn't always make the best decisions on his own. He relied on you to channel his raw strength into action.",
+    "s1": 1,
     "t2": "You always told Brutadon not to stomp on anybody. You said, hold back, Brutadon!",
     "s2": "",
     "t3": "",
@@ -105,7 +105,6 @@ var tutorial = [
     "s6": ""
   },
   {
-    "21": 1,
     "prompt": "But one day, the Pacific ocean churned, and from the dark and mysterious depths, emerged Gromyulox, a kaijew terror even bigger and badder than Brutadon. It cut a swath of destruction across Seattle, Redmond, Kirkland, and Bellview, Washington. When you and Brutadon arrived at the scene, it was a sober moment for both of you. You stood in silence and looked at the destruction.",
     "wreck": 2,
     "pump": 2,
@@ -114,6 +113,7 @@ var tutorial = [
     "none": 1,
     "huh": 2,
     "t1": "You and Brutadon locked eyes. You both knew that this was what you had been training for. His destiny, had arrived.",
+    "s1": 1,
     "t2": "You stood in silence and looked at the destruction.",
     "s2": "",
     "t3": "",
@@ -126,7 +126,6 @@ var tutorial = [
     "s6": ""
   },
   {
-    "21": 1,
     "prompt": "Brutadon followed in Gromyulox's wake of destruction. Soon, the two stood face to face in the skyscraper-filled city of Franklin. Everyone held their breath as the two kaijew prepared to battle. But you knew that Brutadon needed your help to win. Suddenly, Gromyulox leaped forward with his poison fangs bared! Brutadon froze! Should he wreck it? Should he pump it up? Did he need to hear, you got this, Brutadon? Did he need to hold back?",
     "wreck": 1,
     "pump": 2,
@@ -135,6 +134,7 @@ var tutorial = [
     "none": 5,
     "huh": 6,
     "t1": "Brutadon rushes forward with his teeth bared, too! His fangs clank off of Gromyulox's fangs! They each retreat. This will be a difficult fight.",
+    "s1": 1,
     "t2": "Brutadon flexes, and Gromyulox is shocked by both the quantity and quality of his gigantic muscles. He freezes in place, suddenly aware that this will be a very, difficult, fight.",
     "s2": 1,
     "t3": "You know that Brutadon can't do this without your emotional support. He hears your cheer, and is filled with courage! He karate-chops Gromyulox in the neck with one of his giant claws. Gromyulox is stunned. Brutadon will be a much more difficult foe than he expected",
